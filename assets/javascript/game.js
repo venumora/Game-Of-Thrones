716fbb0052011aec80fdda4374fe40dfc95819c6
venu.mora <venukumar.mora@gmail.com>
2017-11-14 02:12:35 -0800

    List of players done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,41 @@
 (function($) {
 	'use strict';
+
+	$('.js-player__me').on('click', function() {
+		var playerText = $(this).find(".player-text").get(0);
+
+		$('.player').addClass('player__enemy');
+		$('.player-text').text("Select your Enemy!!");
+
+		if(playerText) {
+			$(playerText).text("You are!!");
+		}
+
+		$(this).find(".player")
+		.removeClass('player__enemy')
+		.removeClass('js-player__enemy')
+		.removeClass('is-hidden');
+
+		$('.js-player__me').unbind('click')
+		.removeClass('js-player__me')
+		.addClass('js-player__enemy');
+
+		$('.js-player__enemy').on('click', function(){
+			var playerText = $(this).find(".player-text").get(0);
+
+			if(playerText) {
+				$(playerText).text("Your current enemy is!!");
+			}
+
+			$(this).find(".player")
+			.addClass('player__enemy--current')
+			.removeClass('is-hidden');
+
+			$('.js-player__enemy').unbind('click')
+			.removeClass('js-player__enemy');
+		});
+	});
+
 	var gameOfThrones = function() {
 
 		gameOfThrones.prototype.init = () => {
